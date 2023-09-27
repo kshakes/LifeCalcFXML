10092256349959bcf6d716b11b807dab7adc8108
Shakezk <kyleshakes04@gmail.com>
2023-09-27 10:07:03 +0100

    -Changed the text to buttons with transparent backgrounds -Made the window not resizeable -Changed the boxes on screen to a smaller size

--- src/main/java/com/example/lifecalcfxml/HelloApplication.java
+++ src/main/java/com/example/lifecalcfxml/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
