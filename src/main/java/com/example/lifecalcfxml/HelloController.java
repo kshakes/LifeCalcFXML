6dda550a24e70b18b32e5aca502365c667057455
Shakezk <kyleshakes04@gmail.com>
2023-09-28 17:14:40 +0100

    Update - New TextField and Button. Allows you to write anything in there and when the button is pressed, it will replace current placeholder text Need to implement exception handling

--- src/main/java/com/example/lifecalcfxml/HelloController.java
+++ src/main/java/com/example/lifecalcfxml/HelloController.java
@@ -1,5 +1,21 @@
 package com.example.lifecalcfxml;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class HelloController {
 
+    @FXML
+    private TextField salaryNum;
+    @FXML
+    private Text showInfo;
+    public void calc() {
+
+        String salaryText = salaryNum.getText();
+
+        float salary = Integer.parseInt(salaryText);
+
+        showInfo.setText("Monthly = £" + salary/12f);
+    }
 }
