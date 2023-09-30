785e3d032c4600a19cb98979db247afc4ae271fa
Shakezk <kyleshakes04@gmail.com>
2023-09-30 14:21:19 +0100

    Added a new function that sets variables that i can reuse in the HelloController class. The name will be changed later on. Also added how much the user has left after all the expenses

--- src/main/java/com/example/lifecalcfxml/HelloController.java
+++ src/main/java/com/example/lifecalcfxml/HelloController.java
@@ -12,23 +12,39 @@ public class HelloController {
 
     static MoneyManager mm = new MoneyManager();
 
+    private double monthlyNet;
+    private double carBudget;
+    private double houseBudget;
+    private double investmentAmount;
+
     public void calc() {
         if (!salaryNum.getText().equals("")){
             try{
                 double salaryText = Double.parseDouble(salaryNum.getText());
-//              //Set the variables
+
+                //Set the variables
                 mm.setmonthly(salaryText);
                 mm.setCarBudget();
                 mm.setHouseCost();
                 mm.setInvestmentAmount();
 
+                setVars();
+
                 showInfo.setVisible(true);
                 showInfo.setText("Monthly = £" + mm.getmonthly() + "\n\nHouse Budget: £" +
                         mm.getHouseCost() + "\n\nCar Budget: £" + mm.getCarBudget() +
-                        "\n\nInvestment Amount: £" + mm.getInvestmentAmount());
+                        "\n\nInvestment Amount: £" + mm.getInvestmentAmount() +
+                        "\n\nMoney Left: £" + (monthlyNet - carBudget - houseBudget - investmentAmount));
             } catch (NumberFormatException e){
                 throw e;
             }
         }
+    }
+
+    public void setVars(){
+        carBudget = mm.getCarBudget();
+        houseBudget = mm.getHouseCost();
+        investmentAmount = mm.getInvestmentAmount();
+        monthlyNet = mm.getmonthly();
     }
 }
