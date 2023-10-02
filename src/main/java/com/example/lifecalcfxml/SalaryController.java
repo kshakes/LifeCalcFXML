faa1e7f7867a468a1c68e3817a2a3521355c55ed
Kyle <kyleshakes04@gmail.com>
2023-10-02 21:30:05 +0100

    Code is now commented and robust (from my tests)

--- src/main/java/com/example/lifecalcfxml/SalaryController.java
+++ src/main/java/com/example/lifecalcfxml/SalaryController.java
@@ -14,32 +14,43 @@ import javafx.util.Duration;
 import java.text.DecimalFormat;
 
 public class SalaryController {
+    //Variables for Edit Details section
     @FXML
     private HBox editFieldHBox;
     @FXML
     private TextField budgetTypeField;
     @FXML
     private Text budgetTypeText;
-
+    @FXML
+    private Button editDetailsButton;
+    //Variables for inputting the salary and showing the calculations
     @FXML
     private TextField salaryNum;
     @FXML
     private Text showInfo;
-    @FXML
-    private Button editDetailsButton;
 
     static MoneyManager mm = new MoneyManager();
 
+    //Variables that are a part of showInfo
     private double monthlyNet;
     private double carBudget;
     private double houseBudget;
     private double investmentAmount;
-
+    //Helps with formatting and not having .000000000000
     private static final DecimalFormat df = new DecimalFormat("0.00");
-
+    //Key with determining whether the carBudget/houseBudget input field should be visible and determines
+    //Animations that should be played to put it back to its original state
     private boolean isEdited = false;
 
     public void calc() {
+        //Animation settings to fade the showInfo. Creates a smoother UI and better user experience
+        FadeTransition fadeInTransitionField = new FadeTransition(Duration.seconds(0.5), showInfo);
+        fadeInTransitionField.setFromValue(0.0);
+        fadeInTransitionField.setToValue(1.0);
+
+        //If the salaryNum box isn't empty, then check if it isn't on the phase of having car/house budget edited
+        //If it isn't, then allow all the values to be calculated purely from the salary
+        //If it is being edited, then just display the values
         if (!salaryNum.getText().isEmpty()){
             try{
                 if (!isEdited){
@@ -50,13 +61,13 @@ public class SalaryController {
                     showInfo.setVisible(true);
                     editDetailsButton.setVisible(true);
                 }
-
+                fadeInTransitionField.play();
                 showInfo.setText("Monthly = £" + mm.getmonthly() + "\n\nHouse Budget: £" +
                         mm.getHouseBudget() + "\n\nCar Budget: £" + mm.getCarBudget() +
                         "\n\nInvestment Amount: £" + mm.getInvestmentAmount() +
                         "\n\nMoney Left: £" + df.format(monthlyNet - carBudget - houseBudget - investmentAmount));
-            } catch (NumberFormatException e){
-                throw e;
+            } catch (NumberFormatException e){ //Make sure there is only numbers input
+                System.out.println(e);
             }
         }
     }
@@ -94,7 +105,7 @@ public class SalaryController {
             fadeInTransitionField.play();
             editFieldHBox.setVisible(true);
         }
-        else{
+        else{ //Resets the animation after the user has edited the car and house budget
             transition.setToY(0);
             transition.play();
 
@@ -104,11 +115,15 @@ public class SalaryController {
     }
     public void editDetailsConfirm(KeyEvent key) {
         try{
-            if (key.getCode() == KeyCode.ENTER){
-                String checkText = budgetTypeText.getText();
+            if (key.getCode() == KeyCode.ENTER){ // If the user clicks the Enter key
+                String checkText = budgetTypeText.getText(); // Get whatever is in the box (ints only)
+                //In order for this section to work, I figured the easiest way is to check that the text = the section you edit
+                //and then change it once it's edited. I found it alot easier to track when things are edited and the code
+                //is readable
                 if (checkText.contains("Car Budget:")){
                     carBudget = Double.parseDouble(budgetTypeField.getText());
                     mm.setCarBudget(carBudget);
+                    budgetTypeField.clear(); //Clear it so the user doesn't have to remove their previous input
                     budgetTypeText.setText("House Budget:");
                     isEdited = true;
                 }
