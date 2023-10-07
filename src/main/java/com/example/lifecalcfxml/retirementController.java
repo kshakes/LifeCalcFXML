753c72d8c30606baf30d262a730e1d756ca00a95
Shakezk <kyleshakes04@gmail.com>
2023-10-07 12:05:04 +0100

    Code currently calculated compound interest for the users personal investment amount based off salary, retirement age - users age

--- src/main/java/com/example/lifecalcfxml/retirementController.java
+++ src/main/java/com/example/lifecalcfxml/retirementController.java
@@ -1,24 +1,41 @@
 package com.example.lifecalcfxml;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 
 public class retirementController {
 
     @FXML
     private TextField ageTextField;
 
-    final int retirementAge = 66;
+    final double averageReturn = 0.08 / 12;
+    double initialBalance = 0;
 
     public void calcRetirement() {
+
         try{
             int userAge = Integer.parseInt((ageTextField.getText()));
+
             if (userAge >= 66){
                 System.out.println("You are already at or above the retirement age");
-            }
-            System.out.println(userAge);
+            } else{
 
+                int months = (66-userAge) * 12;
+                System.out.println(months);
+
+                for (int i = 1; i < months; i++){ //For every year in retirement age - years
+                    //Do monthly compound interest
+                    initialBalance = (initialBalance + SalaryController.investmentAmount) * (1 + averageReturn);
+                    if (i % 12 == 0){
+                        System.out.println("Year : " + i / 12 + " -> Balance: " + SalaryController.df.format(initialBalance));
+                    }
+                }
+            }
         } catch (InputMismatchException e){
             throw e;
         }
